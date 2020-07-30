f30151f38a9ff0903975058d6c60b7a668c6c8d7
jjswift <supercheater2018@yandex.ru>
2020-07-30 23:30:12 +0300

    hamburget, footer and woman adaptive

--- scripts/header.js
+++ scripts/header.js
@@ -1,4 +1,13 @@
 document.querySelector('.toggle-icon').onclick = () => {
     document.querySelector('#nav-container').classList.toggle("pushed");
-    console.log('bruh')
+    if (document.querySelector('#nav-container').classList.contains('pushed')) {
+        document.querySelector('.nav-mob').style.display = 'flex'
+        document.querySelector('.close-menu').style.display = 'block'
+        document.querySelector('.open-menu').style.display = 'none';
+    } else {
+        document.querySelector('.nav-mob').style.display = 'none'
+
+        document.querySelector('.close-menu').style.display = 'none'
+        document.querySelector('.open-menu').style.display = 'block';
+    }
 }
